486a693f4d13c3e7b6a95b3a96af498034880eeb
Jacob Walls <jwalls@fargeo.com>
2025-05-03 07:43:40 +0100

    Add multicolumn index on TileModel

--- arches/app/models/models.py
+++ arches/app/models/models.py
@@ -1718,6 +1718,14 @@ class TileModel(SaveSupportsBlindOverwriteMixin, models.Model):  # Tile
     class Meta:
         managed = True
         db_table = "tiles"
+        indexes = [
+            models.Index(
+                # Order nodegroup first to avoid separately indexing nodegroup.
+                "nodegroup",
+                "resourceinstance",
+                name="nodegroup_and_resource",
+            )
+        ]
 
     def __repr__(self):
         return f"<{self.find_nodegroup_alias()} ({self.pk})>"
